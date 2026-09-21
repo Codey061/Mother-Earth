70ce88c70192cf76666e9c461ce3d2a61bef538c
KingDude061 <81912258+KingDude061@users.noreply.github.com>
2026-09-21 16:06:16 -0400

    Safeguard against duplicate years

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function ClickHello(){
-    alert("Hello");
-}
+
 function Calculate(){
     const start = document.getElementById("startDate").value;
     const end = document.getElementById("endDate").value;
     const data = 11 * (end - start);
-    if(data<0){
-    alert('Please input the years in order');
-    return;
+    if(data<=0){
+        if(data<0){
+            alert('Please input the years in order.');
+            return;
+        }else{
+            alert('Make sure the 2 years aren\'t the same year');
+        }
     }
     alert('The ocean gained ' + data + ' million tonnes of microplastic between those 2 years'+((data>100)?'!':'.'));
 }
